f782401af478e603eb06472e41117c022ffda37a
Максим <78905554+F-u-n-H-o-u-s-e@users.noreply.github.com>
2022-05-27 08:37:35 +0300

    Add rulse for game

--- JavaScript_Game-Guess_the_number.js
+++ JavaScript_Game-Guess_the_number.js
@@ -1,53 +1,65 @@
-'use strict';
+"use strict";
 let Random = (min, max) => Math.round(Math.random() * (max - min) + min);
 let winner = Random(1, 20);
 let Point = 20;
 let TheBest = 0;
 function MessageDispley(message) {
-  document.querySelector('.guess-message').textContent = message;
+  document.querySelector(".guess-message").textContent = message;
 }
 
 function Game() {
-  let TRY = Number(document.querySelector('.number-input').value);
+  let TRY = Number(document.querySelector(".number-input").value);
 
   if (!TRY) {
-    MessageDispley('Right number');
+    MessageDispley("Right number");
     // No number
   } else if (TRY !== winner) {
     if (Point > 1) {
-      document.querySelector('.guess-message').textContent =
-        TRY > winner ? 'Too Hight' : 'Too smole';
+      document.querySelector(".guess-message").textContent =
+        TRY > winner ? "Too Hight" : "Too smole";
       Point = Point - 1;
     } else {
-      document.querySelector('.guess-message').textContent = 'Game Over';
+      document.querySelector(".guess-message").textContent = "Game Over";
       Point = 0;
     }
     // Too small
   } else if (TRY === winner) {
-    document.querySelector('.guess-message').textContent = 'Right!';
-    document.querySelector('.question').textContent = TRY;
-    document.querySelector('body').style.background = 'green';
-    document.querySelector('.question').style.fontSize = '10rem';
+    document.querySelector(".guess-message").textContent = "Right!";
+    document.querySelector(".question").textContent = TRY;
+    document.querySelector("body").style.background = "green";
+    document.querySelector(".question").style.fontSize = "10rem";
     if (Point > TheBest) {
       TheBest = Point;
-      document.querySelector('.highscore').textContent = TheBest;
+      document.querySelector(".highscore").textContent = TheBest;
     }
     // Win
   }
-  document.querySelector('.score').textContent = Point;
+  document.querySelector(".score").textContent = Point;
 }
 
 function Again() {
-  document.querySelector('body').style.background = 'black';
-  document.querySelector('.question').textContent = '???';
-  document.querySelector('.question').style.fontSize = '4rem';
-  document.querySelector('.guess-message').textContent = 'Начни Угадывать!';
+  document.querySelector("body").style.background = "black";
+  document.querySelector(".question").textContent = "???";
+  document.querySelector(".question").style.fontSize = "4rem";
+  document.querySelector(".guess-message").textContent = "Начни Угадывать!";
   Point = 20;
-  document.querySelector('.score').textContent = Point;
+  document.querySelector(".score").textContent = Point;
   winner = Random(1, 20);
   console.log(winner);
-  document.querySelector('.number-input').value = '';
+  document.querySelector(".number-input").value = "";
 }
 
-document.querySelector('.check').addEventListener('click', Game);
-document.querySelector('.again').addEventListener('click', Again);
+function DelClass() {
+  document.querySelector(".Bg-for-button").classList.remove("Hide");
+  document.querySelector(".Ruls-Text").classList.remove("Hide");
+}
+
+function AddClass() {
+  document.querySelector(".Bg-for-button").classList.add("Hide");
+  document.querySelector(".Ruls-Text").classList.add("Hide");
+}
+
+document.querySelector(".check").addEventListener("click", Game);
+document.querySelector(".again").addEventListener("click", Again);
+document.querySelector(".ruls").addEventListener("click", DelClass);
+document.querySelector(".Btn-Close").addEventListener("click", AddClass);
